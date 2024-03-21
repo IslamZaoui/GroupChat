cb195e7c71e377e1d41b2e4b070df2c04da768d8
IslamZaoui <zaoui.islam2021@gmail.com>
2024-03-21 01:38:58 +0100

    Fix pocketbase client sync with server side

--- src/hooks.client.ts
+++
@@ -1,6 +0,0 @@
-import { pb } from '$lib/pocketbase'
-
-pb.authStore.loadFromCookie(document.cookie)
-pb.authStore.onChange(() => {
-    document.cookie = pb.authStore.exportToCookie({ httpOnly: false })
-}, true)
--- src/lib/pocketbase/index.ts
+++ src/lib/pocketbase/index.ts
@@ -1,8 +1,51 @@
+import { browser } from '$app/environment';
 import { env } from '$env/dynamic/public';
-import PocketBase from 'pocketbase'
+import PocketBase, { BaseAuthStore, cookieParse, type AuthModel } from 'pocketbase'
 
-export function createInstance() {
-  return new PocketBase(env.PUBLIC_POCKETBASE_URL)
+class CookieStore extends BaseAuthStore {
+  constructor() {
+    super();
+    if (browser)
+      this.loadFromCookie(document.cookie)
+  }
+
+  get token() {
+    if (browser)
+      try {
+        const data = JSON.parse(cookieParse(document.cookie)['pb_auth']);
+        return data.token
+      } catch (_) { }
+
+    return super.token;
+  }
+
+  get model() {
+    if (browser)
+      try {
+        const data = JSON.parse(cookieParse(document.cookie)['pb_auth']);
+        return data.model
+      } catch (_) { }
+
+    return super.model;
+  }
+
+  save(token: string, model: AuthModel | undefined) {
+    super.save(token, model)
+
+    if (browser)
+      document.cookie = this.exportToCookie({ httpOnly: false })
+  }
+
+  clear() {
+    super.clear()
+
+    if (browser)
+      document.cookie = this.exportToCookie({ httpOnly: false })
+  }
 }
 
-export const pb = createInstance()
+export function createInstance(store?: CookieStore) {
+  return new PocketBase(env.PUBLIC_POCKETBASE_URL, store)
+}
+
+export const pb = createInstance(new CookieStore())

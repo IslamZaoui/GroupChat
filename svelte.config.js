9790534d4a21f4a627667e749032aee4ac204cb3
IslamZaoui <zaoui.islam2021@gmail.com>
2024-03-30 21:16:56 +0100

    Bun

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from 'svelte-adapter-bun';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,7 +7,8 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			runtime: 'edge'
+			assets: true,
+			dynamic_origin: true
 		}),
 		alias: {
 			'@/*': './src/lib'
